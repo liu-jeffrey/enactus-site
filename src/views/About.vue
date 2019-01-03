f2f7639d5486bde026e9fc9ce032f019373c6db7
Jeffelini <jeffreyliu0117@gmail.com>
2019-01-03 16:19:21 -0500

    built additional router pages

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <div class="columns">
+      <div class="column"><h1>THIS IS COLUMN 1</h1></div>
+      <div class="column"><h1>THIS IS COLUMN 2</h1></div>
+      <div class="column">
+        <h1>THIS IS COLUMN 3</h1>
+        <p>HIHIHIH</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "about"
+  // components: {
+  //   HelloWorld
+  // }
+};
+</script>
+
+<style lang="scss">
+.columns {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
